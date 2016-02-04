03fd14d3f2d10443dcc10ff65d2dbc6a5313d421
irwinghg <irwinghg@gmail.com>
2016-02-04 17:26:08 -0600

    Prueba Irwin

---
+++ ControlNotas/ControlNotas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ControlNotas.Startup))]
+namespace ControlNotas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
